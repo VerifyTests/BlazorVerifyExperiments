9ea2d793ef7a0dc034292fe87d4f8540ebf371c4
SimonCropp <simon.cropp@gmail.com>
2020-04-25 16:44:20 +1000

    Verify login page rendering

---
+++ BandBooker_03_Complete/BandBookerWasm/Client.Tests/LoginPageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using BandBookerWasm.Client.Pages;
+using BandBookerWasm.Client.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using VerifyBunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class LoginPageTests :
+    VerifyBase
+{
+    [Fact]
+    public Task Simple()
+    {
+        var navigationMock = new Mock<NavigationManager>();
+        var authMock = new Mock<IAuthService>();
+        Services.Add(ServiceDescriptor.Singleton(provider => navigationMock.Object));
+        Services.Add(ServiceDescriptor.Singleton(provider => authMock.Object));
+        var component = RenderComponent<Login>();
+        var model = component.Instance.LoginModel;
+        model.Email = "a@b.com";
+        model.Password = "thePassword";
+        component.Render();
+        return Verify(component);
+    }
+
+    public LoginPageTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
